879f0d21b64b2e31c8ce2dc2a9320e61ad5209af
Gad Ishimwe <coolshigad@gmail.com>
2020-01-28 12:40:31 +0200

    chore(redux): add redux setup
    - Add redux and react-redux
    
    [starts #169817474]

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App/App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import user from './store/reducers/user';
+
+const rootReducer = combineReducers({
+    user
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(<Provider store={store}><App /></Provider >, document.getElementById('root'));
