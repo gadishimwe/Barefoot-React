63f935fdc2739bca842e3e069a001426079094af
Gustave Harintwari <higustave123@gmail.com>
2020-02-20 10:14:28 +0200

    implement trips requests table Apply recent rebase Test reducers Add new features Rename file Rename file enam efiles Apply recent rebase Apply rebase Fix routes problem Fix routes Fix heroku image Fix routes errors Fix tests failure Fix heroku images loading Fix

--- src/views/tripRequests.js
+++ src/views/tripRequests.js
@@ -142,7 +142,14 @@ export default function tripRequests() {
               My Trip Requests
             </h2>
           </Grid>
-          {currentTrips.map(trip => {
+        {(currentTrips.length === 0 ? 
+          <div style={{ paddingTop: '20%', textAlign: 'center', color: '#979696 ', width:'100%'}}>
+            <h2 style={{ margin: 'auto'}}>
+              No Trips Requests Found
+            </h2>
+          </div>
+          :
+          (currentTrips.map(trip => {
             let {
               tripType,
               originId,
@@ -203,7 +210,8 @@ export default function tripRequests() {
                 />
               </Grid>
             )
-          })
+          }))
+          )
           }
           <Grid
             item
